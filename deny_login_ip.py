f63d3407251c8c8f114e7a04ea0014b4a505b78d
罗长法 <luocfa@users.noreply.github.com>
2020-01-03 10:36:15 +0800

    Update deny_login_ip.py

--- deny_login_ip.py
+++ deny_login_ip.py
@@ -30,6 +30,8 @@ def monitorLog(logFile):
     popen = subprocess.Popen('tail -f ' + logFile, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     # 开始监控
     while True:
+        # 1s10次吧
+        time.sleep(0.1)
         # 按行读
         line = popen.stdout.readline().strip()
         if line:
@@ -40,6 +42,7 @@ def monitorLog(logFile):
                 subprocess.getoutput('echo \'sshd:{}\' >> {}'.format(group[1], hostDeny))
                 deniedDict[group[1]] = '1'
                 print('add ip:{} to hosts.deny for invalid user'.format(group[1]))
+                continue
 
             # 用户名合法 密码错误的
             group = re.search(r'Failed password for \w+ from (\d+\.\d+\.\d+\.\d+) ', str(line))
@@ -50,13 +53,13 @@ def monitorLog(logFile):
                     tempIp[ip] = 1
                 else:
                     tempIp[ip] = tempIp[ip] + 1
-                # 密码错误次数大于阈值的时候，直接拉黑，理论上
+                # 密码错误次数大于阈值的时候，直接拉黑
                 if tempIp[ip] > passwd_wrong_num and not deniedDict.get(ip):
                     del tempIp[ip]
                     subprocess.getoutput('echo \'sshd:{}\' >> {}'.format(ip, hostDeny))
                     deniedDict[ip] = '1'
                     print('add ip:{} to hosts.deny for invalid password'.format(ip))
-        time.sleep(1)
 
 if __name__ == '__main__':
     monitorLog(logFile)
+
